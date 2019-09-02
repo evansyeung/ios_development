1f8c33e71ea151a7d6d89e771d23d2936ae151d0
Evans Yeung <evansbyeung@gmail.com>
2019-09-02 18:41:27 -0400

    Initial projects

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Evans Yeung on 8/29/19.
+//  Copyright © 2019 Evans Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
